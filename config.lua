338d27a60fc9c9300d305b6412d8cfab9af863d1
Anonymas <157628039+Anoymasproject@users.noreply.github.com>
2024-03-06 16:19:05 -0500

    Update Config

--- config.lua
+++ config.lua
@@ -1,4 +1,3 @@
--- Configuration principale
 Config = {
     Job = "winemaker",
     Locale = "en",
@@ -6,7 +5,7 @@ Config = {
     Price = {min = 12, max = 30},
 }
 
--- Configuration des blips
+-- Blips Configuration
 Config.Blips = {
     Harvest = {Pos = {x = -1886.0090, y = 2221.3091, z = 90.4437}, BlipName = "Harvest"},
     Pressing = {Pos = {x = -1931.8933, y = 2055.3696, z = 140.7537}, BlipName = "Pressing"},
@@ -14,7 +13,7 @@ Config.Blips = {
     Selling = {Pos = {x = -1220.68, y = -910.77, z = 12.07}, BlipName = "Sale"},
 }
 
--- Configuration des zones
+-- Zones Configuration
 Config.Zones = {
     Collected = vec3(-1879.9648, 2109.2786, 136.1772),
     Pressing = vec3(-1928.7947, 2059.8057, 140.8171),
@@ -22,7 +21,7 @@ Config.Zones = {
     Sale = vec3(-1887.8220, 2050.9001, 140.9968),
 }
 
--- Configuration des items
+-- Items Configuration
 Config.Items = {
     Grapes = {Red = "red_grapes", White = "white_grapes", Pink = "pink_grapes"},
     PressedGrapes = {Red = "red_grapes_pressed", White = "white_grapes_pressed", Pink = "pink_grapes_pressed"},
@@ -31,7 +30,7 @@ Config.Items = {
     EmptyCan = "empty_wine_can",
 }
 
--- Fonction pour obtenir le nom complet d'un blip
+-- Function to get the full name of a blip
 function GetBlipFullName(category)
     return "Winery - " .. Config.Blips[category].BlipName
 end
